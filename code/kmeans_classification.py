885e5c3d1613f05e24629ea4323ce2118d4698ed
assanethiao <thiaoassanethiao@gmail.com>
2026-06-08 10:00:47 +0200

    commit 08/06/2026 a 10h00

--- code/kmeans_classification.py
+++ code/kmeans_classification.py
@@ -13,35 +13,19 @@ gt = np.load('..\\dataset\\IPgt.npy')
 # Preprocessing
 M, N, K = data.shape
 
-'''
-new_data = np.copy(data)
-
-for i in range(1, M-1):
-    for j in range(1, N-1):
-
-        new_data[i,j,:] = (
-            data[i,j,:]      # centre
-            + data[i-1,j,:]  # haut
-            + data[i+1,j,:]  # bas
-            + data[i,j-1,:]  # gauche
-            + data[i,j+1,:]  # droite
-        ) / 5
-
-X = new_data.reshape(M * N, K)
-'''
+data_pad = np.pad(data, ((1,1),(1,1),(0,0)), mode='reflect')
 features = []
 
-for i in range(1, M-1):
-    for j in range(1, N-1):
+for i in range(1, M+1):
+    for j in range(1, N+1):
 
-        centre = data[i,j,:]
-        haut   = data[i-1,j,:]
-        bas    = data[i+1,j,:]
-        gauche = data[i,j-1,:]
-        droite = data[i,j+1,:]
+        centre = data_pad[i,j,:]
+        haut   = data_pad[i-1,j,:]
+        bas    = data_pad[i+1,j,:]
+        gauche = data_pad[i,j-1,:]
+        droite = data_pad[i,j+1,:]
 
         feature = np.concatenate([centre, haut, bas, gauche, droite])
-
         features.append(feature)
 
 X = np.array(features)
@@ -85,7 +69,7 @@ for n_clusters in range(2, 17):
 '''
 
 # K-means clusturing
-n_clusters =  16 # à tester
+n_clusters =  15 # à tester
 kmeans = KMeans(n_clusters=n_clusters, random_state=0, n_init=20)
 labels = kmeans.fit_predict(X)
 
